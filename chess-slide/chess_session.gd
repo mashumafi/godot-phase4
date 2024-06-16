85cee0c18615f35221dd9b01676fecb8e6e8f4d8
mashumafi <mashumafi@gmail.com>
2024-06-16 12:22:24 -0400

    Available moves optiized and add UI

---
+++ chess-slide/chess_session.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _flip_board() -> void:
+	%Chess2D.is_flipped = not %Chess2D.is_flipped
--- chess-slide/new_script.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-func _flip_board() -> void:
-	$Chess2D.is_flipped = not $Chess2D.is_flipped
