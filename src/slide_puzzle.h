78c40ce6b951ef10d453a777e009bd8e7480c369
mmurphy316 <mmurphy316@bloomberg.net>
2025-04-14 17:12:03 -0400

    format

--- src/slide_puzzle.h
+++ src/slide_puzzle.h
@@ -1,6 +1,6 @@
 #include <godot_cpp/classes/object.hpp>
-#include <godot_cpp/classes/ref_counted.hpp>
 #include <godot_cpp/classes/random_number_generator.hpp>
+#include <godot_cpp/classes/ref_counted.hpp>
 
 #include <godot_cpp/variant/array.hpp>
 
@@ -13,11 +13,11 @@ protected:
 	static void _bind_methods();
 
 public:
-	static PackedVector2Array shuffle(int p_complexity, Array p_squares, int p_moves, const Ref<RandomNumberGenerator>& p_rng);
+	static PackedVector2Array shuffle(int p_complexity, Array p_squares, int p_moves, const Ref<RandomNumberGenerator> &p_rng);
 	static bool is_solvable(int p_complexity, const PackedInt32Array &p_squares);
 	static PackedVector2Array solve(int p_complexity, const PackedInt32Array &p_squares);
 
-	void test(Array&){}
+	void test(Array &) {}
 };
 
 } //namespace godot
